a3a75aea54dde082f0c9455cc58ddb0231c64b14
kumahua <40682280+kumahua@users.noreply.github.com>
2020-05-20 13:29:28 +0800

    uploadtest

---
+++ PhotonPun+/Photon Unity Networking/Demos/DemoRPGMovement/Scripts/DemoRPGMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoRPGMovement : MonoBehaviour 
+{
+    public RPGCamera Camera;
+
+    void OnJoinedRoom()
+    {
+        CreatePlayerObject();
+    }
+
+    void CreatePlayerObject()
+    {
+        Vector3 position = new Vector3( 0.0f, -11.0f, 0.0f ); //33.5f, 1.5f, 20.5f
+
+        GameObject newPlayerObject = PhotonNetwork.Instantiate( "Robot Kyle RPG", position, Quaternion.identity, 0 );
+
+        Camera.Target = newPlayerObject.transform;
+    }
+}
